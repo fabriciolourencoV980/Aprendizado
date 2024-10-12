83567616f85ecd40ebb70dbd56c48cc6bc934fba
Fabrício Lourenco da Silva <lourencofabricio824@gmail.com>
2024-10-12 14:44:55 -0300

    Teste02

---
+++ Teste/Teste02.js
@@ -0,0 +1,16 @@
+//Trabalhando com váriaveis: 
+
+var nome = "Caneta"
+var quantidade = 10;
+var preco = 6.4;
+var imposto = 1.5;
+var precoFinal = preco + imposto; 
+
+console.log(nome);
+console.log(quantidade);
+console.log(preco );
+console.log(imposto);
+console.log(precoFinal);
+
+nome = "Caneta BIC";
+console.log(nome);
