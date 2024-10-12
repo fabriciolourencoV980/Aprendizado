1145f6b37168654baa0ce1f38eb012e88a445752
Fabrício Lourenco da Silva <lourencofabricio824@gmail.com>
2024-10-12 17:09:41 -0300

    Create Teste07.js

---
+++ Teste/Teste07.js
@@ -0,0 +1,11 @@
+// troca de valores... Sendo a >>> 94 e b >>> 7
+
+let a = 7;
+let b = 94;
+let temp = a;  // Se criou uma terceira variavel que armazena um valor temporário neste caso 7 conforme a variavel a. 
+a = b; 
+b = temp; 
+
+
+console.log(`Valor de a: ${a} \n Valor de b: ${b}`);
+
