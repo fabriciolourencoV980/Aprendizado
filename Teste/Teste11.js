8df8443f83cd94811ce918a0a6d7b802b1273547
Fabrício Lourenco da Silva <lourencofabricio824@gmail.com>
2024-10-13 19:27:16 -0300

    Teste11.js

---
+++ Teste/Teste11.js
@@ -0,0 +1,10 @@
+//Template String
+
+const nome = 'Rebeca';
+const concatenacao = 'Olá' + nome + '!';
+
+console.log(`Olá ${nome}!`);
+console.log(`1 + 1 = ${1 + 1}`);
+
+const up = texto => texto.toUpperCase();
+console.log(`Ei... ${up('cuidado')}!`); 
