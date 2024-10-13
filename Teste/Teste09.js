575d20dcbc869307ea7f00860d24e6c4921dcb37
Fabrício Lourenco da Silva <lourencofabricio824@gmail.com>
2024-10-13 18:58:46 -0300

    Teste09.js

---
+++ Teste/Teste09.js
@@ -0,0 +1,15 @@
+console.log(7 / 0);
+console.log("10" / 2);
+console.log("Show!" * 2); 
+console.log(0.1 + 0.7);
+//console.log(10.toString)
+console.log((10.345).toFixed(2));
+
+//Utilizando o Math
+//O método Math.pow() em JavaScript é utilizado para calcular a potência de um número, ou seja, ele eleva um número base a uma certa potência.
+
+const raio = 5.6;
+const area = Math.PI * Math.pow(raio, 2);
+
+console.log(area.toFixed(2));
+console.log(typeof Math);
