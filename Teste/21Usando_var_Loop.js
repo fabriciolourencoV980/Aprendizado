52a49f490390a07a6cceb872ba1705fa430961e2
Fabrício Lourenco da Silva <lourencofabricio824@gmail.com>
2024-10-16 09:55:13 -0300

    .

---
+++ Teste/21Usando_var_Loop.js
@@ -0,0 +1,6 @@
+// O laço for em JavaScript é uma estrutura de controle usada para repetir um bloco de código um número específico de vezes. Ele é frequentemente utilizado quando você sabe com antecedência quantas vezes deseja executar o bloco de código.
+
+for (var i = 0; i < 10; i++){
+    console.log(i); 
+};
+console.log('i =', i);
