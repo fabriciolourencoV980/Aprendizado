d49ff89cfda9a7982e84e0f0d5d4e99a618deb00
Fabrício Lourenco da Silva <lourencofabricio824@gmail.com>
2024-10-21 10:08:14 -0300

    .

---
+++ Teste/28Operador_destructuring.js
@@ -0,0 +1,22 @@
+// Novo recurso do ES2015
+
+const pessoa = {
+    nome: 'Ana',
+    idade: 5,
+    endereco:{
+        logradouro: 'Rua ABC',
+        numero: '100',
+    }
+}; 
+
+const { nome, idade } = pessoa; // Tire do objeto pessoa nome e a idade
+console.log(nome , idade ); 
+
+const { nome: n, idade: i} = pessoa; 
+console.log(n,i);
+
+const { sobrenome, bemHumorada = null} = pessoa;
+console.log(sobrenome, bemHumorada);
+
+const { endereco: {logradouro, numero, cep}} = pessoa;
+console.log(logradouro, numero, cep);
